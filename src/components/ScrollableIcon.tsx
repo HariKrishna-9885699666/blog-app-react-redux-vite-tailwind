25b5e486884c3e09a11fa5e2ac1edf90e0ce2c81
Hari Krishna Anem <hari-krishna@Haris-MacBook-Pro.local>
2024-05-27 14:54:39 +0530

    fixed the tabindex

--- src/components/ScrollableIcon.tsx
+++ src/components/ScrollableIcon.tsx
@@ -40,7 +40,7 @@ const ScrollableIcon: React.FC = () => {
       {modalIsOpen && (
         <div
           id="default-modal"
-          tabIndex="-1"
+          tabIndex={-1}
           aria-hidden="true"
           className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
         >
