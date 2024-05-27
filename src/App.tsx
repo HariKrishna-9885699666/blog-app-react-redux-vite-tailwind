36585ebb41f56aaf8f8be1ee0621ba1e37ba26d4
Hari Krishna Anem <hari-krishna@Haris-MacBook-Pro.local>
2024-05-27 15:20:03 +0530

    updated the title

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
       <nav className="bg-blue-600 p-4">
         <div className="container mx-auto">
           <h1 className="text-white text-3xl font-bold text-center">
-            Blog Post CRUD Operations with React, Vite, Redux, and Tailwind CSS
+            Blog Post CRUD Operations with React, Vite, Redux, Typescript and Tailwind CSS
           </h1>
         </div>
       </nav>
