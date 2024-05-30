0b70c0aacb1abcd761eb836cd845fdc6a9a3f47f
Hari Krishna Anem <hari-krishna@Haris-MacBook-Pro.local>
2024-05-30 15:10:31 +0530

    added documentation

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from './slices/postSlice'; 
 
+/**
+ * Configures the Redux store with the posts reducer.
+ */
 const store = configureStore({
-    reducer: {
-        posts: postsReducer,
-    },
+  reducer: {
+    posts: postsReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
